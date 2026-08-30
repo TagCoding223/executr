0179791cc5d347b2c11f22f076049850a46fe0dd
Vishal Chouhan <vishalchouhanv.c22.3@gmail.com>
2026-08-30 23:34:28 +0530

    Initial monorepo commit

---
+++ backend/src/main/java/com/executr/ExecutrApplication.java
@@ -0,0 +1,13 @@
+package com.executr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecutrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExecutrApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/executr/ExecutrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.executr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecutrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
