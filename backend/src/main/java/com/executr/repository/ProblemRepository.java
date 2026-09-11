5a00946038f413be99c82006f333cdb71df1e4d9
Vishal Chouhan <vishalchouhanv.c22.3@gmail.com>
2026-09-11 08:10:50 +0530

    (backend) create minor not affected changes

--- backend/src/main/java/com/executr/repository/ProblemRepository.java
+++ backend/src/main/java/com/executr/repository/ProblemRepository.java
@@ -4,11 +4,9 @@ import com.executr.entity.Problem;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
     // Used for the URL routing, e.g., /problem/two-sum
     Optional<Problem> findBySlug(String slug);
--- backend/src/main/java/com/executr/repository/UserStatsRepository.java
+++ backend/src/main/java/com/executr/repository/UserStatsRepository.java
@@ -2,8 +2,6 @@ package com.executr.repository;
 
 import com.executr.entity.UserStats;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserStatsRepository extends JpaRepository<UserStats, Long> {
 }
